2a849076b40a8f3d348e09375be4e9408fafe1e8
Evandson <evandson.cm@gmail.com>
2020-07-03 22:43:20 -0300

    Criando interface

--- lib/Home.dart
+++ lib/Home.dart
@@ -6,8 +6,70 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  TextEditingController _tituloController = TextEditingController();
+  TextEditingController _descricaoController = TextEditingController();
+
+  _cadastro() {
+    showDialog(
+        context: context,
+        builder: (context){
+          return AlertDialog(
+            title: Text("Criar anotação"),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextField(
+                  controller: _tituloController,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                      labelText: "Título",
+                      hintText: "Digite o título"
+                  ),
+                ),
+                TextField(
+                  controller: _descricaoController,
+                  decoration: InputDecoration(
+                      labelText: "Descrição",
+                      hintText: "Digite a Descrição"
+                  ),
+                )
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Cancelar"),
+              ),
+              FlatButton(
+                onPressed: () {
+
+                },
+                child: Text("Salvar"),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Anotações"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          child: Icon(Icons.add),
+          onPressed: (){
+            _cadastro();
+          }
+      ),
+    );
   }
 }
